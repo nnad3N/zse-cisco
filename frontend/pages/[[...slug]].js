663c343ae68b26211cec11fcaaa8eabcd762bd6d
Damian Struk <nnad3n@outlook.com>
2021-09-17 23:43:42 +0200

    project cleanup

---
+++ frontend/pages/[[...slug]].js
@@ -0,0 +1,56 @@
+import { fetchAPI } from '../utils/api';
+import Layout from '../components/Layout';
+import Seo from '../components/seo';
+import Sections from '../components/sections';
+import { getPageData } from '../utils/getPageData';
+
+const DynamicPage = ({ sections, pageContext, navigation, slug }) => {
+  //   const seo = {
+  //     metaTitle: category.name,
+  //     metaDescription: `All ${category.name} articles`,
+  //   }
+  console.log(slug);
+  return (
+    <Layout pageContext={pageContext} navigation={navigation}>
+      {/* <Seo seo={seo} /> */}
+      <Sections sections={sections} />
+    </Layout>
+  );
+};
+
+export async function getStaticPaths() {
+  const pages = await fetchAPI('/pages');
+
+  const paths = pages.map((page) => {
+    // Decompose the slug that was saved in Strapi
+    const slugArray = !page.slug ? false : page.slug.split('/');
+
+    return {
+      params: { slug: slugArray },
+    };
+  });
+
+  return { paths, fallback: true };
+}
+
+export async function getStaticProps({ params }) {
+  const pageData = await getPageData({ slug: !params.slug ? [''] : params.slug });
+  const navigation = await fetchAPI('/navigation');
+
+  if (pageData == null) {
+    // Giving the page no props will trigger a 404 page
+    return { props: {} };
+  }
+
+  const { content, slug } = pageData;
+
+  return {
+    props: {
+      sections: content,
+      navigation,
+      slug,
+    },
+  };
+}
+
+export default DynamicPage;
