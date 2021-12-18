8f00513e7bb74e0bce8fe25dec72cbb0a168267f
Damian Struk <nnad3n@outlook.com>
2021-12-18 18:00:15 +0100

    Migrated from strapi v3 to v4 - clean

--- backend.old/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1338),
-  admin: {
-    auth: {
-      secret: env("ADMIN_JWT_SECRET", "518feef0b3d0427fe8eaa85cff9392b4"),
-    },
-  },
-});
